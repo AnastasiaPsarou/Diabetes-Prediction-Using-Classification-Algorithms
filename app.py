577f0ac6d12e0d21ad607ec28eea5e559f89357d
Anastasia <anastasiapsarou123@gmail.com>
2023-05-09 10:45:14 +0300

    change of results

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from flask import Flask, render_template, request
 from flask_ngrok import run_with_ngrok
 import pickle
 import pandas as pd
-import numpy
-
+import numpy as np
 
 app = Flask(__name__)
 run_with_ngrok(app)  
@@ -14,33 +13,31 @@ model = pickle.load(open('model.pkl', 'rb'))
 @app.route('/')
 def home():
     result = ''
-    return render_template('index.html', **locals())
+    return render_template('index.html', result=result)
 
-
-@app.route('/predict', methods=['POST', 'GET'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    Number_of_Pregrancies = float(request.form['Number of Pregrancies'])
+    Number_of_Pregnancies = float(request.form['Number_of_Pregnancies'])
     Glucose = float(request.form['Glucose'])
-    Blood_Pressure = float(request.form['Blood Pressure'])
-    Skin_Thickness = float(request.form['Skin Thickness'])
+    Skin_Thickness = float(request.form['Skin_Thickness'])
     Insulin = float(request.form['Insulin'])
     BMI = float(request.form['BMI'])
-    Diabetes_Pedigree_Function = float(request.form['Diabetes Pedigree Function'])
+    Diabetes_Pedigree_Function = float(request.form['Diabetes_Pedigree_Function'])
     Age = float(request.form['Age'])
     
-    data = [[Number_of_Pregrancies, Glucose, Blood_Pressure, Skin_Thickness, Insulin, BMI, Diabetes_Pedigree_Function, Age]]
+    data = [[Number_of_Pregnancies, Glucose, Skin_Thickness, Insulin, BMI, Diabetes_Pedigree_Function, Age]]
   
     # Create the pandas DataFrame with column name is provided explicitly
-    df = pd.DataFrame(data, columns=['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age'])
+    df = pd.DataFrame(data, columns=['Pregnancies', 'Glucose', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age'])
     
     result = model.predict(df)
     
-    if(result == numpy.array([0])):
+    if result == np.array([0]):
         result = "You don't have diabetes"
     else:
         result = "You have diabetes"
     
-    return render_template('index.html', **locals())
+    return render_template('index.html', result=result)
 
 if __name__ == '__main__':
     app.run()
